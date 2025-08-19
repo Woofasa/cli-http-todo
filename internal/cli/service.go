58d806af14214861e0fd5f59cb006bf1d8d0692a
Woofasa <woofasa132@gmail.com>
2025-08-19 04:00:34 +0300

    Added service logic: add, exit, delete.

--- internal/cli/service.go
+++ internal/cli/service.go
@@ -1,34 +1,79 @@
 package cli
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"main/internal/domain"
 	"main/internal/repo"
 	"main/internal/repo/json_storage"
+	"os"
+	"os/exec"
+	"strings"
 )
 
 type Service struct{}
 
 func Run() error {
 	taskList := domain.NewTaskList()
-
 	jsonstorage := json_storage.JSONStorage{}
 	repo := repo.Repository{
 		DB: map[string]repo.StorageManager{
 			"json": jsonstorage,
 		},
 	}
-	repo.LoadAll("json", taskList)
 
-	if err := taskList.CreateTask("Выгуливать кошку", "Надеть намордник обязательно"); err != nil {
-		log.Fatal(err)
+	if err := repo.LoadAll("json", taskList); err != nil {
+		log.Fatalf("loaging error: %v", err)
 	}
 
-	fmt.Println("Список задач:")
-	for i, v := range taskList.NumeratedSort() {
-		fmt.Println(i+1, v.Title)
-	}
+	running := true
+	for running {
+		numerated := taskList.NumeratedSort()
+		fmt.Println("Task list:")
+		for i, v := range numerated {
+			fmt.Println(i+1, v.Title)
+		}
+		var choice string
+		fmt.Println("\"exit\" | \"delete\" | \"add\"")
+		fmt.Print("Make one of the commands: ")
+		fmt.Scan(&choice)
+		switch strings.ToLower(choice) {
+		case "exit":
+			running = false
+		case "delete":
+			fmt.Print("Task number for deleting: ")
+			var number int
+			fmt.Scan(&number)
+			taskList.RemoveTask(numerated[number].ID)
+			repo.SaveAll(taskList)
+			clear()
+		case "add":
+			title, desc := addHandler()
+			taskList.CreateTask(title, desc)
+			repo.SaveAll(taskList)
+			clear()
+		default:
+			fmt.Println("Unknown command")
+		}
 
+	}
 	return nil
+}
+
+func addHandler() (title, desc string) {
+	reader := bufio.NewScanner(os.Stdin)
+	fmt.Print("Task title: ")
+	reader.Scan()
+	title = reader.Text()
+	fmt.Print("Task description: ")
+	reader.Scan()
+	desc = reader.Text()
+	return
+}
+
+func clear() {
+	cmd := exec.Command("clear")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
 }
